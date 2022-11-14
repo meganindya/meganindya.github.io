2547d6f2a6656f50899b07c3819a1449b639ab71
Anindya Kundu <anindyaak007@gmail.com>
2022-11-15 00:35:49 +0530

    refactor: reorganise code for project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
+<!-- == SCRIPT ============================================================= -->
+
 <script setup lang="ts"></script>
+
+<!-- == TEMPLATE =========================================================== -->
 
 <template>Portfolio</template>
 
-<style scoped lang="scss"></style>
+<!-- == STYLE ============================================================== -->
+
+<style lang="scss">
+@import '@/scss/colors.scss';
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  position: relative;
+  font-weight: normal;
+}
+
+body {
+  min-height: 100vh;
+  margin: 0;
+
+  line-height: 1.6;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+    Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-size: 15px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: var(--color-text);
+  background-color: var(--color-background);
+  //   transition: color 0.5s, background-color 0.5s;
+
+  #app {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 0 2rem;
+    font-weight: normal;
+
+    a {
+      text-decoration: none;
+    }
+  }
+}
+</style>
