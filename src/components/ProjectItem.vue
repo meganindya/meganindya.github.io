92c289826ecf8af1300b6b7943d702e1376d9522
Anindya Kundu <anindyaak007@gmail.com>
2022-11-16 04:15:27 +0530

    feat: update projects

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -7,11 +7,34 @@ import ImageCarousel from '@/components/ImageCarousel.vue';
 
 const props = defineProps({
   title: String,
+  tech: Array<string>,
   desc: String,
-  images: Array<string>
+  links: Array<string>
 });
 
-const { title, desc, images } = toRefs(props);
+const { title, tech, desc, links } = toRefs(props);
+
+function getBadgeURL(name: string) {
+  function asURL() {
+    let url = '';
+
+    if (name.split('-').length > 0) {
+      url += name.split('-').join('');
+    }
+
+    while (url.indexOf(' ') !== -1) {
+      url = url.replace(/ /, '%20');
+    }
+
+    while (url.indexOf('+') !== -1) {
+      url = url.replace(/\+/, '%2B');
+    }
+
+    return url;
+  }
+
+  return `https://img.shields.io/badge/-${asURL()}-000?&logo=${asURL()}`;
+}
 </script>
 
 <!-- == TEMPLATE =========================================================== -->
@@ -19,10 +42,17 @@ const { title, desc, images } = toRefs(props);
 <template>
   <div class="project-item markdown-body">
     <h2 v-html="title"></h2>
+
+    <div class="project-item-badge-list">
+      <template v-for="(name, i) in tech" :key="`tech-${i}`">
+        <img :src="getBadgeURL(name)" :alt="name" />
+      </template>
+    </div>
+
     <p v-html="desc"></p>
 
-    <template v-if="images!.length > 0">
-      <ImageCarousel :images="images" />
+    <template v-if="links!.length > 0">
+      <ImageCarousel :images="links" />
     </template>
   </div>
 </template>
@@ -36,5 +66,13 @@ const { title, desc, images } = toRefs(props);
 
   border-radius: 8px;
   background-color: var(--c-bg-subtle);
+
+  .project-item-badge-list {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+
+    margin-bottom: 0.5rem;
+  }
 }
 </style>
