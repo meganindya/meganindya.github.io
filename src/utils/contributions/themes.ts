47e4dd35d71146044388fc74b5f0aafe49d4c2c3
Anindya Kundu <anindyaak007@gmail.com>
2022-11-17 01:22:19 +0530

    feat(views): [PageProfile] add GitHub contributions graph

---
+++ src/utils/contributions/themes.ts
@@ -0,0 +1,22 @@
+export const themes = {
+    light: {
+        background: '#ffffff',
+        text: '#24292f',
+        meta: '#666666',
+        grade4: '#216e39',
+        grade3: '#30a14e',
+        grade2: '#40c463',
+        grade1: '#9be9a8',
+        grade0: '#f6f8fa'
+    },
+    dark: {
+        background: '#0d1117',
+        text: '#c9d1d9',
+        meta: '#dddddd',
+        grade4: '#27d545',
+        grade3: '#10983d',
+        grade2: '#00602d',
+        grade1: '#003820',
+        grade0: '#161b22'
+    }
+};
